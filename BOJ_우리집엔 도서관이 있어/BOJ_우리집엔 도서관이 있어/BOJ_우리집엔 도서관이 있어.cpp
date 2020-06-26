2d85b6951e00b11ea04edd2e801da648399eac9a
unknown <Hg-lab>
2020-06-26 17:35:18 +0900

    20200626

---
+++ BOJ_우리집엔 도서관이 있어/BOJ_우리집엔 도서관이 있어/BOJ_우리집엔 도서관이 있어.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+int n;
+int temp;
+vector<int> v;
+
+int main()
+{
+	cin >> n;
+	
+	for (int i = 0; i < n; i++) {
+		cin >> temp;
+		v.push_back(temp);
+	}
+	int cur = n;
+
+	for (int i = n - 1; i >= 0; i--) {
+		if (v[i] == cur) cur--;
+	}
+
+	cout << cur;
+
+}
