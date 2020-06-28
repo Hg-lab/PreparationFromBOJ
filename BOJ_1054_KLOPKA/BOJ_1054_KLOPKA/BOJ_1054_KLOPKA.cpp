443e6b22a94c5288b520a6fce4fb6c70535972dc
unknown <Hg-lab>
2020-06-28 23:14:36 +0900

    20200628

---
+++ BOJ_1054_KLOPKA/BOJ_1054_KLOPKA/BOJ_1054_KLOPKA.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <cmath>
+#include <algorithm>
+
+using namespace std;
+const int MAX = 99999999;
+const int MIN = 0;
+int main()
+{
+	int n;
+	cin >> n;
+	int minX = MAX, minY = MAX, maxX = MIN, maxY = MIN;
+	for (int i = 0; i < n; i++) {
+		int x, y;
+		cin >> x >> y;
+		if (x < minX) minX = x;
+		if (y < minY) minY = y;	
+		if (x >= maxX) maxX = x;
+		if (y >= maxY) maxY = y;
+	}
+	int diffX = maxX - minX;
+	int diffY = maxY - minY;
+	int answer = max(diffX, diffY);
+
+	answer *= answer;
+
+	cout << answer;
+}
---
+++ BOJ_1748_수 이어 쓰기1/BOJ_1748_수 이어 쓰기1/BOJ_1748_수 이어 쓰기1.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <cmath>
+
+using namespace std;
+int main()
+{
+	int n;
+	cin >> n;
+	long long int count = 0;
+	long long int answer = 0;
+
+	// 몇자리 수
+	int temp = n;
+	while (temp / 10 >= 1) {
+		if (temp / 10 >= 1) {
+			temp /= 10;
+			count++;
+		}
+	} // n : count + 1 의 자리 수
+
+	for (int i = 1; i <= count; i++) {
+		answer += 9 * i * pow(10, i - 1); // 120 : 1*9*pow(10,0), 2*9*(10,1)개 = 9+180 = 189
+	}
+	answer += (count + 1)*(n - pow(10,count) + 1); // 3 * (120-100 + 1)개, count = 2; = 
+
+	cout << answer;
+
+
+}
